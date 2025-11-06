3b1ccf2837c4bc7020f5871258488c56817ba461
dileep0524 <dileepkaibadi2405@gmail.com>
2025-11-06 11:06:56 +0530

    added url
    Signed-off-by: dileep0524 <dileepkaibadi2405@gmail.com>

--- server/http.go
+++ server/http.go
@@ -42,6 +42,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 			"http://localhost:8000",
 			"http://localhost:3000",
 			"http://localhost:5173", // Vite default
+			"http://localhost:8083", // Admin dashboard
 		}
 
 		// Check if origin is allowed
